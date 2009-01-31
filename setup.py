46e4dda74a5a4e55240ed15501c2b878b56ede58
Martijn Faassen <faassen@startifact.com>
2009-01-31 14:48:46 +0000

    remove dev marker.

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='zope.proxy',
-      version = '3.5.0dev',
+      version = '3.5.0',
       author='Zope Corporation and Contributors',
       author_email='zope-dev@zope.org',
       description='Generic Transparent Proxies',
