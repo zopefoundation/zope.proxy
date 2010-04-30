bb80455cd609fb2c09b69513f1ee6137e4a8af60
Hanno Schlichting <hanno@hannosch.eu>
2010-04-30 21:28:13 +0000

    Prepare zope.proxy 3.6.0.

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='zope.proxy',
-      version = '3.6.0dev',
+      version = '3.6.0',
       author='Zope Corporation and Contributors',
       author_email='zope-dev@zope.org',
       description='Generic Transparent Proxies',
