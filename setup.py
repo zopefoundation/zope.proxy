008d15ffad27f5fd34159529bbca4ec0daae78e1
Tres Seaver <tseaver@palladion.com>
2012-06-06 21:21:20 +0000

    Prep 4.0.0 release.

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ else:
     features = {'Cwrapper': Cwrapper}
 
 setup(name='zope.proxy',
-      version = '4.0.0dev',
+      version = '4.0.0',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
       description='Generic Transparent Proxies',
