788cb7935771157fa621875d60bb143cef93d7a4
Marius Gedminas <marius@gedmin.as>
2019-11-11 20:13:37 +0200

    Back to development: 4.3.4

--- setup.py
+++ setup.py
@@ -84,7 +84,7 @@ else:
     features = {'Cwrapper': Cwrapper}
 
 setup(name='zope.proxy',
-      version='4.3.3',
+      version='4.3.4.dev0',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
       description='Generic Transparent Proxies',
