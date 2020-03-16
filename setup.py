7c24ab0850b4973d68a37a084ebe3ce3bf80b73b
Marius Gedminas <marius@gedmin.as>
2020-03-16 18:05:15 +0200

    Back to development: 4.3.6

--- setup.py
+++ setup.py
@@ -80,7 +80,7 @@ else:
     headers = [os.path.join('src', 'zope', 'proxy', 'proxy.h')]
 
 setup(name='zope.proxy',
-      version='4.3.5',
+      version='4.3.6.dev0',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
       description='Generic Transparent Proxies',
