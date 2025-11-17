47e27d03e105c8be3f4234a68121e2b2169451fd
Jens Vagelpohl <jens@plyp.com>
2025-11-17 07:14:56 +0100

    - Drop Python 3.9, add 3.14, move metadata to ``pyproject.toml``

--- setup.py
+++ setup.py
@@ -30,9 +30,6 @@ from setuptools import setup
 from setuptools.command.build_ext import build_ext
 
 
-version = '7.1.dev0'
-
-
 class optional_build_ext(build_ext):
     """This class subclasses build_ext and allows
        the building of C extensions to fail.
@@ -60,11 +57,6 @@ class optional_build_ext(build_ext):
         print('*' * 80)
 
 
-def read(*rnames):
-    with open(os.path.join(os.path.dirname(__file__), *rnames)) as f:
-        return f.read()
-
-
 codeoptimization = [
     Extension(
         "zope.proxy._zope_proxy_proxy",
@@ -73,75 +65,13 @@ codeoptimization = [
 ]
 
 # PyPy won't build the extension.
-is_pypy = platform.python_implementation() == 'PyPy'
-if is_pypy:
+if platform.python_implementation() == 'PyPy':
     ext_modules = []
     headers = []
 else:
     ext_modules = codeoptimization
     headers = [os.path.join('src', 'zope', 'proxy', 'proxy.h')]
 
-setup(name='zope.proxy',
-      version=version,
-      author='Zope Foundation and Contributors',
-      author_email='zope-dev@zope.dev',
-      description='Generic Transparent Proxies',
-      long_description=(
-          read('README.rst')
-          + '\n\n' +
-          read('CHANGES.rst')
-      ),
-      url='http://github.com/zopefoundation/zope.proxy',
-      project_urls={
-          'Documentation': 'https://zopeproxy.readthedocs.io',
-          'Issue Tracker': 'https://github.com/zopefoundation/'
-                           'zope.proxy/issues',
-          'Sources': 'https://github.com/zopefoundation/zope.proxy',
-      },
-      license='ZPL-2.1',
-      classifiers=[
-          'Development Status :: 5 - Production/Stable',
-          'Intended Audience :: Developers',
-          'License :: OSI Approved :: Zope Public License',
-          'Programming Language :: Python',
-          'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.9',
-          'Programming Language :: Python :: 3.10',
-          'Programming Language :: Python :: 3.11',
-          'Programming Language :: Python :: 3.12',
-          'Programming Language :: Python :: 3.13',
-          'Programming Language :: Python :: Implementation :: CPython',
-          'Programming Language :: Python :: Implementation :: PyPy',
-          'Framework :: Zope :: 3',
-          'Natural Language :: English',
-          'Operating System :: OS Independent',
-      ],
-      keywords='proxy generic transparent',
-      # we need the following two parameters because we compile C code,
-      # otherwise only the shared library is installed:
-      package_dir={'': 'src'},
-      packages=['zope.proxy'],
-      cmdclass={
-          'build_ext': optional_build_ext,
-      },
+setup(cmdclass={'build_ext': optional_build_ext},
       headers=headers,
-      ext_modules=ext_modules,
-      python_requires='>=3.9',
-      install_requires=[
-          'zope.interface',
-      ],
-      include_package_data=True,
-      zip_safe=False,
-      extras_require={
-          'test': [
-              # We have a circular dependency with zope.security for testing
-              'zope.security >= 7.3',
-              'zope.testrunner >= 6.4',
-          ],
-          'docs': [
-              'Sphinx',
-              'repoze.sphinx.autointerface',
-              'sphinx_rtd_theme',
-          ],
-      },
-      )
+      ext_modules=ext_modules)
